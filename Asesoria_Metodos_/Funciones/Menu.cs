7147630f481d423b4a56c81295187e2136b48f64
AngelDv18 <133921931+AngelDv18@users.noreply.github.com>
2023-06-16 13:56:00 -0500

    xd

---
+++ Asesoria_Metodos_/Funciones/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asesoria_Metodos_.Funciones
+{
+    internal class Menu
+    {
+        public Menu()
+        {
+            //Console.WriteLine("Ingresa otro numero");//
+            //int n3 = int.Parse(Console.ReadLine());//
+            Console.WriteLine("Ingresa una de las opciones");
+            Console.WriteLine("1) Suma");
+            Console.WriteLine("2) Resta");
+            Console.WriteLine("3) Multiplicación");
+            Console.WriteLine("4) Division");
+            Console.WriteLine("5) Salir del programa");
+            
+        }
+    }
+}
+
--- Asesoria_Metodos_/Program.cs
+++ Asesoria_Metodos_/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Channels;
+﻿using Asesoria_Metodos_.Funciones;
+using System;
+using System.ComponentModel.Design;
 
 namespace Asesoria_Metodos_
 {
@@ -54,7 +54,6 @@ namespace Asesoria_Metodos_
             //}
             #endregion
 
-
             #region--CONDICION SWITCH--
             //Console.WriteLine("Seleciona un dia de la semana");
             //string opcion = (Console.ReadLine());
@@ -82,46 +81,8 @@ namespace Asesoria_Metodos_
             //}
             #endregion
 
-
-            //Console.WriteLine("Ingresa un numero");
-            //int n1 = int.Parse(Console.ReadLine());
-            //Console.WriteLine("Ingresa otro numero");
-            //int n2 = int.Parse(Console.ReadLine());
-            //int resultado= int.Parse(Console.ReadLine));
-            //Console.Clear();
-            //Console.WriteLine("Ingresa una de las opciones");
-            //Console.WriteLine("1) Suma");
-            //Console.WriteLine("2) Resta");
-            //Console.WriteLine("3) Salir del programa");
-            //int opcion = int.Parse(Console.ReadLine());
-
-            //switch (opcion)
-            //{
-            //    case 1:
-            //        Console.Clear();
-            //        resultado = n1 + n2;
-            //        Console.WriteLine("El resultado de la suma es: " + resultado);
-            //        break;
-            //    case 2:
-            //        Console.Clear();
-            //        resultado = n1 - n2;
-            //        Console.WriteLine("El resultado de la resta es: " + resultado);
-            //        break;
-            //    case 3:
-            //        Console.Clear();
-            //        Console.WriteLine("Saliste del programa");
-            //        break;
-            //    default:
-            //        Console.WriteLine("opcion invalida. Selecciona otra occion");
-            //        break;
-            //}
-
-            //public static int SumaDosNumeros( int n1, int n2)
-            //{
-            //    int resultado = n1 + n2;
-            //    return resultado;
-            //}
+            Menu menu = new Menu();
+            Operaciones operaciones = new Operaciones();
         }
     }
 }
-    
