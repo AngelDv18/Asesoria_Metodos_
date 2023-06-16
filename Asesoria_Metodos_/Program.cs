8a609c5472dd8162ccda606123e62965cdabdaca
AngelDv18 <133921931+AngelDv18@users.noreply.github.com>
2023-06-16 12:25:45 -0500

    Agregar archivos de proyecto.

---
+++ Asesoria_Metodos_/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace Asesoria_Metodos_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        //tipos de operaciones = operaciones aritmeticas, operaciones condicionales, operaciones logicas.
+        {
+            Console.WriteLine("Hola mundo");
+            #region--Condicion if--
+            //Console.WriteLine("Seleciona un dia de la semana");
+            //string opcion = Console.ReadLine();
+
+
+            //if (opcion == "Lu")
+            //{
+            //    Console.WriteLine("Seleccionaste Lunes ");
+            //}
+            //else
+            //{
+            //    if (opcion == "Ma")
+            //    {
+            //        Console.WriteLine("Seleccionaste Martes ");
+            //    }
+            //    else
+            //    {
+            //        if (opcion == "Mi")
+            //        {
+            //            Console.WriteLine("Sleccionaste Miercoles");
+            //        }
+            //        else
+            //        {
+            //            if (opcion == "Ju")
+            //            {
+            //                Console.WriteLine("Sleccionaste Jueves");
+            //            }
+            //            else
+            //            {
+            //                if (opcion == "Vi")
+            //                {
+            //                    Console.WriteLine("Sleccionaste Miercoles");
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("Seleciona una opcion valida");
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            #endregion
+
+
+            #region--CONDICION SWITCH--
+            //Console.WriteLine("Seleciona un dia de la semana");
+            //string opcion = (Console.ReadLine());
+            //switch (opcion)
+            //{
+            //    case "A":
+            //        Console.WriteLine("Dia lunes");
+            //        break;
+            //    case "B":
+            //        Console.WriteLine("Dia Martes");
+            //        break;
+            //    case "C":
+            //        Console.WriteLine("Dia Miercoles");
+            //        break;
+            //    case "D":
+            //        Console.WriteLine("Dia Jueves");
+            //        break;
+            //    case "E":
+            //        Console.WriteLine("Dia Viernes");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("No Existe lo que ingresate");
+            //        break;
+            //}
+            #endregion
+
+
+            //Console.WriteLine("Ingresa un numero");
+            //int n1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingresa otro numero");
+            //int n2 = int.Parse(Console.ReadLine());
+            //int resultado= int.Parse(Console.ReadLine));
+            //Console.Clear();
+            //Console.WriteLine("Ingresa una de las opciones");
+            //Console.WriteLine("1) Suma");
+            //Console.WriteLine("2) Resta");
+            //Console.WriteLine("3) Salir del programa");
+            //int opcion = int.Parse(Console.ReadLine());
+
+            //switch (opcion)
+            //{
+            //    case 1:
+            //        Console.Clear();
+            //        resultado = n1 + n2;
+            //        Console.WriteLine("El resultado de la suma es: " + resultado);
+            //        break;
+            //    case 2:
+            //        Console.Clear();
+            //        resultado = n1 - n2;
+            //        Console.WriteLine("El resultado de la resta es: " + resultado);
+            //        break;
+            //    case 3:
+            //        Console.Clear();
+            //        Console.WriteLine("Saliste del programa");
+            //        break;
+            //    default:
+            //        Console.WriteLine("opcion invalida. Selecciona otra occion");
+            //        break;
+            //}
+
+            //public static int SumaDosNumeros( int n1, int n2)
+            //{
+            //    int resultado = n1 + n2;
+            //    return resultado;
+            //}
+        }
+    }
+}
+    
